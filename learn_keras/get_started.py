2923f450aca56050e7fab94ed82b12e14ab2efe5
Guang-Tong Zhou <zhouguangtong@gmail.com>
2016-09-10 18:52:14 -0700

    Add get_started.py

---
+++ learn_keras/get_started.py
@@ -0,0 +1,11 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Activation
+
+
+model = Sequential([
+    Dense(32, input_dim=784),
+    Activation('relu'),
+    Dense(10),
+    Activation('softmax')
+])
