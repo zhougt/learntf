7b297518d07dbbae45ed2bf99ebe6e20c454ce27
Guang-Tong Zhou <zhouguangtong@gmail.com>
2016-10-24 23:48:27 -0700

    Change main

--- catdog/main.py
+++ catdog/main.py
@@ -14,7 +14,7 @@ nb_train_samples = 2000
 nb_validation_samples = 800
 nb_epoch = 50
 
-
+# with tf.device("/gpu:0"):
 model = Sequential()
 model.add(Convolution2D(32, 3, 3, input_shape=(img_width, img_height, 3)))
 model.add(Activation('relu'))
@@ -35,36 +35,37 @@ model.add(Dropout(0.5))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='rmsprop',
+    metrics=['accuracy'])
 
 # this is the augmentation configuration we will use for training
 train_datagen = ImageDataGenerator(
-        rescale=1. / 255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
 
 # this is the augmentation configuration we will use for testing:
 # only rescaling
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=32,
-        class_mode='binary')
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=32,
+    class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=32,
-        class_mode='binary')
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=32,
+    class_mode='binary')
 
 model.fit_generator(
-        train_generator,
-        samples_per_epoch=nb_train_samples,
-        nb_epoch=nb_epoch,
-        validation_data=validation_generator,
-        nb_val_samples=nb_validation_samples)
+    train_generator,
+    samples_per_epoch=nb_train_samples,
+    nb_epoch=nb_epoch,
+    validation_data=validation_generator,
+    nb_val_samples=nb_validation_samples)
